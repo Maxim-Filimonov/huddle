5089c70e3e71fef9956adfdb93a8b87e7b0e4f44
Maxim Filimonov <tpaktopsp@gmail.com>
2013-04-25 19:31:00 +1000

    3.3 chapter done

--- app/models/status_report.rb
+++ app/models/status_report.rb
@@ -1,4 +1,15 @@
 class StatusReport < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  before_save :set_status_date
+  validate :validate_has_at_least_one_status
+
+  def set_status_date
+    self.status_date = Date.today unless self.status_date
+  end
+  def validate_has_at_least_one_status
+    if today.blank? && yesterday.blank?
+      errors[:base] << "Must have at least one status set"
+    end
+  end
 end
--- test/models/status_report_test.rb
+++ test/models/status_report_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class StatusReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'saving a status report saves the status date' do
+    actual = StatusReport.new today: 't', yesterday: 'y'
+    actual.save
+    assert_equal(Date.today.to_s, actual.status_date.to_s)
+  end
+  test "saving a status report that has a date doesn't override it" do
+    expected_date = 2.days.ago.to_date
+    actual = StatusReport.new :status_date => expected_date, today: 't', yesterday: 'y'
+    actual.save
+    actual.reload
+    assert_equal(expected_date.to_s, actual.status_date.to_s)
+  end
+  test 'a report with both blank statuses blank is not valid' do
+    actual = StatusReport.new today: '', yesterday: ''
+    assert !actual.valid?
+  end
+  test 'a report with yesterday blank is valid' do
+    actual = StatusReport.new today: 'toda', yesterday: ''
+    assert actual.valid?
+  end
+  test 'a report with today blank is valid' do
+    actual = StatusReport.new today: '', yesterday: 'yesterday'
+    assert actual.valid?
+  end
 end
